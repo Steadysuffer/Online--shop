c66958c611672235b3cd9892fb4a684d6aafeb5d
Steadysuffer <Blad111222333@mail.ru>
2023-06-13 13:02:33 +0300

    Добавлено наследование от базового html-файла

--- products/views.py
+++ products/views.py
@@ -4,8 +4,35 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return render(request, 'products/index.html', {'title': 'BIG SMOKE'})
+    context = {
+        'title': 'Магазин одежды',
+        'is_promotion': True,
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html', {'title': 'PRODUCTS'})
+    context = {
+        'title': 'Раздел товаров',
+        'products': [
+            {
+                'image':'/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description':'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+            {
+                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+                'name': 'Синяя куртка The North Face',
+                'price': 23725,
+                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+            },
+            {
+                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
+                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                'price': 3390,
+                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+            }
+        ]
+    }
+    return render(request, 'products/products.html', context)
